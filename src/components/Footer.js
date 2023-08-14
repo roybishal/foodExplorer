951404bd9811ba59f12426b43e3672ab1f17a06c
roybishal <bishalroy2210@gmail.com>
2023-08-14 16:32:24 +0530

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
 
     // footer section
-    <div  data-testid="footer-test" className='font-thin  text-xl min-[300px]:text-sm  font-serif shadow-2xl rounded-xl shadow-slate-300 p-4 mt-10 mb-5 mx-10   text-red-600
+    <div  data-testid="footer-test" className='  text-xl   font-serif  rounded-xl shadow-slate-300 p-4 mt-10 mb-5 mx-10   text-red-600
     m-auto  text-center bg-pink-200' >
   
-    <h2 > Developed with 💛 by Bishal Roy  </h2>  
+     Developed with 💛 by Bishal Roy 
   
     
     </div>
