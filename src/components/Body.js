2d98e65c1464a123e0c975c1659f650f5d57d8f7
roybishal <bishalroy2210@gmail.com>
2023-08-14 16:27:27 +0530

    updated urls to work without CORS extension

--- src/components/Body.js
+++ src/components/Body.js
@@ -17,7 +17,7 @@ const Body = () => {
     },[]);
 
     async function getRestaurants(){
-        const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.488300272096538&lng=88.32804770688476&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const data= await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.488300272096538&lng=88.32804770688476&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
          //console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
          //console.log(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);       
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const IMG_CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-export const RES_MENU_API= "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
+export const RES_MENU_API= "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
